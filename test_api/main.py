1c5aba72abccbc0ba4e4a44b26c935baa4e15341
Maria Rosenhaus <maria.rosenhaus@gmail.com>
2025-10-08 05:13:58 +0300

    Завершена структура функции request_to_neural_network.

--- test_api/main.py
+++ test_api/main.py
@@ -1,10 +1,12 @@
 import sys
 from pathlib import Path
 
+import requests
 
 from .take_the_key import get_key
 
 key_for_neural_network = None
+
 def key_is_active(key):
     """
     Здесь проверяем активен ли ключ.
@@ -24,10 +26,62 @@ def request_to_neural_network(words_list):
     if not key_is_active(key_for_neural_network):
         key_for_neural_network = get_key()  # получаем новый ключ только если нужно
 
-    """
-    Здесь запросы к нейросети. 
-    И возможно формирование таблицы.
-    """
+    if not key_is_active(key_for_neural_network):
+        print("Ключ отсутствует или некорруктен.")
+        return None
+
+    url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+    headers = {
+        "Authorization": f"Bearer {key_for_neural_network}",
+        "Content-Type": "application/json"
+    }
+
+    table = []
+
+    for word in words_list:
+        prompt = f"Дай транскрипцию с ударением и перевод слова {word}. Формат ТРАНСКРИПЦИЯ - ПЕРЕВОД"
+        data = {
+            "modelUri": "gpt://b1gd0j1qrlj8e90tbbfp/yandexgpt",
+            "completionOptions": {
+                "temperature": 0.3,
+                "maxTokens": 100
+            },
+            "messages": [{"role": "user", "text": prompt}]
+        }
+
+        response = requests.post(
+            url=url,
+            headers=headers,
+            json=data
+        )
+
+        if response.status_code == 200:
+            answer = response.json()
+            alternatives = answer.get("result", {}).get("alternatives", [])
+            if len(alternatives) > 0:
+                result = alternatives[0].get("message", {}).get("text", None)
+                if result == None:
+                    print(f"Слово {word}: неккоректный ответ.")
+                    continue
+            else:
+                print(f"Слово {word}: неккоректный ответ.")
+                continue
+
+            #result = answer["result"]["alternatives"][0]["message"]["text"]
+
+            parts = result.split("-")
+            if len(parts) == 2:
+                transcription = parts[0].strip()
+                translation = parts[1].strip()
+                table.append([word, transcription, translation])
+            else:
+                print(f"Слово {word} неверный формат.")
+        else:
+            print(f"Ошибка: {response.status_code}")
+            continue
+
+
+    return table
 
 
 
