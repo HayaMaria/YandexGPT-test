30a781927125a1d8857c0ed60bb9188326ce46b8
Maria <maria.rosenhaus@gmail.com>
2025-10-16 16:24:53 +0300

    Исправление take_the_key: изменен путь к файлам. Легкие правки в README.md.

--- test_api/take_the_key.py
+++ test_api/take_the_key.py
@@ -4,48 +4,51 @@ import requests
 import json
 import jwt
 import time
+import os
 
 
 def get_key():
-    # Путь к файлам secret.json и token_cache.txt в корне проекта
-    secret_path = Path(__file__).resolve().parent.parent / "secrets.json"
-    token_cache_path = Path(__file__).resolve().parent.parent / "token_cache.txt"
+    # Путь к файлам secret.json и token_cache.
+    folder = os.getenv("KEY_FOLDER")
+    base_path = folder or os.path.expanduser("~")
+    token_cache_path = Path(base_path) / "token_cache.txt"
+    secret_path = Path(base_path) / "secrets.json"
 
-    if not token_cache_path.exists():  # Проверяем, существует ли файл
-        token_cache_path.touch()       # Если файла нет - создаём пустой
+    if not token_cache_path.exists():  # Проверяем, существует ли файл.
+        token_cache_path.touch()  # Если файла нет - создаём пустой.
 
-    # Проверяем содержимое файла token_cache
+    # Проверяем содержимое файла token_cache.
     try:
-        with open(token_cache_path, "r") as f:
+        with token_cache_path.open("r", encoding="utf-8") as f:
             cache = json.load(f)
     except json.JSONDecodeError:
-        cache = {"iam_token": "",  # Заглушка
+        cache = {"iam_token": "",  # заглушка
                  "expiresAt": "1970-01-01T00:00:00Z"
                  }
 
     iam_token = cache.get("iam_token")  # токен
     expiresAt = cache.get("expiresAt")  # срок действия
 
-    # Буффер, чтобы успеть использовать токен до истечения срока
+    # Буффер, чтобы успеть использовать токен до истечения срока.
     buffer_seconds = 10
-    # Переводим expiresAt в формат UNIX-времени
+    # Переводим expiresAt в формат UNIX-времени.
     expiresAt_time = int(datetime.fromisoformat(expiresAt.replace("Z", "+00:00")).timestamp())
 
     now = int(time.time())
 
-    if now < expiresAt_time - buffer_seconds:  # Проверяем, активен ли старый токен
+    if now < expiresAt_time - buffer_seconds:  # Проверяем, активен ли старый токен.
         return iam_token
 
-    # Если старый токен неактивен - создаем новый
+    # Если старый токен неактивен - создаем новый.
 
-    with open(secret_path, "r") as f:  # Читаем содержимое файла secrets
+    with secret_path.open("r", encoding="utf-8") as f:  # Читаем содержимое файла secrets.
         service_key = json.load(f)
 
     payload = {
         "aud": "https://iam.api.cloud.yandex.net/iam/v1/tokens",
         "iss": service_key["service_account_id"],
         "iat": now,
-        "exp": now + 360  # Время жизни токена
+        "exp": now + 360  # Время жизни токена.
         }
 
     jwt_token = jwt.encode(
@@ -54,15 +57,15 @@ def get_key():
         algorithm=service_key["key_algorithm"]
     )
 
-    response = requests.post(  # Запрашиваем новый IAM-токен
+    response = requests.post(  # Запрашиваем новый IAM-токен.
         "https://iam.api.cloud.yandex.net/iam/v1/tokens",
         json={"jwt": jwt_token}
     )
 
-    if response.status_code != 200:  # Проверяем, был ли запрос успешен
+    if response.status_code != 200:  # Проверяем, был ли запрос успешен.
         raise RuntimeError(f"Ошибка запроса: {response.status_code}, {response.text}")
 
-    # Извлекаем токен и срок его действия
+    # Извлекаем токен и срок его действия.
     iam_token = response.json()["iamToken"]
     expiresAt = response.json()["expiresAt"]
     cache = {
@@ -70,11 +73,11 @@ def get_key():
         "expiresAt": expiresAt
         }
 
-    with open(token_cache_path, "w") as f:  # Сохраняем токен и срок его действия в файл
+    with token_cache_path.open("w", encoding="utf-8") as f:  # Сохраняем токен и срок его действия в файл
         json.dump(cache, f)
 
     return iam_token
 
 
-if __name__ == "__main__":  # Тестовый запуск функции
+if __name__ == "__main__":  # Тестовый запуск функции.
     print("Токен: ", get_key())
