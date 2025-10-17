fb0e1a40d3cd429afbfa1d05130d25f56d4f643f
Maria <maria.rosenhaus@gmail.com>
2025-10-17 20:51:29 +0300

    Сделанна логика вывода. Исправленна request_to_neural_network, закончена take_the_words, добавлена мини функция handle_fail. И намеченна writing_to_files.

--- test_api/main.py
+++ test_api/main.py
@@ -1,30 +1,47 @@
-import sys
 from pathlib import Path
-
+import os
 import requests
-
-from .take_the_key import get_key
+from take_the_key import get_key
 
 url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
 modelUri = "gpt://b1gd0j1qrlj8e90tbbfp/yandexgpt"
-key_for_neural_network = None
+
+failed_words = []
+
 
 def take_the_words():
-    """
-    Здесь берём слова у пользователя, и возвращаем их в виде списка.
-    """
-    return list(words)
+    # Путь к файлу words.txt
+    folder = os.getenv("KEY_FOLDER")
+    base_path = folder or os.path.expanduser("~")
+    words_path = Path(base_path) / "words.txt"
+
+    words_list = []
+
+    with words_path.open("r", encoding="utf-8") as f:  # Читаем файл words.txt.
+        for line in f:
+            line = line.strip()  # Убираем пробелы и переносы строк.
+            if not line:         # Пропускаем пустые строки.
+                continue
+
+            words_in_line = line.split(",")     # Разделяем по запятым.
+            for word in words_in_line:
+                word = word.strip()             # Убираем пробелы вокруг слова.
+                if word:                        # Пропускаем пустые.
+                    words_list.append(word)     # Добавляем слово в список.
+
+    return words_list
+
+
+def handle_fail(word, reason):
+    global failed_words
+    failed_words.append(word)
+    print(f"Слово {word}: {reason}.")
+    return
+
 
 def request_to_neural_network(words_list):
 
-
-
-
-
-
-
-
-
+    key_for_neural_network = get_key()
 
     headers = {
         "Authorization": f"Bearer {key_for_neural_network}",
@@ -50,19 +67,18 @@ def request_to_neural_network(words_list):
             json=data
         )
 
+        # TODO: упростить условную часть кода для лучшей читаемости (слишком громоздко и запутанно)
         if response.status_code == 200:
             answer = response.json()
             alternatives = answer.get("result", {}).get("alternatives", [])
             if len(alternatives) > 0:
                 result = alternatives[0].get("message", {}).get("text", None)
-                if result == None:
-                    print(f"Слово {word}: неккоректный ответ.")
+                if result is None:
+                    handle_fail(word, "некорректный ответ")
                     continue
             else:
-                print(f"Слово {word}: неккоректный ответ.")
+                handle_fail(word, "некорректный ответ")
                 continue
-
-            #result = answer["result"]["alternatives"][0]["message"]["text"]
 
             parts = result.split("-")
             if len(parts) == 2:
@@ -70,16 +86,22 @@ def request_to_neural_network(words_list):
                 translation = parts[1].strip()
                 table.append([word, transcription, translation])
             else:
-                print(f"Слово {word} неверный формат.")
+                handle_fail(word, "неверный формат")
         else:
-            print(f"Ошибка: {response.status_code}")
+            handle_fail(word, response.status_code)
             continue
-
 
     return table
 
 
+def writing_to_files(table, failed_word):
+    """Зпись table в файл table_words.txt.
+       Подсчет failed_words и запись о их количесве в файл table_words.txt.
+       Запись failed_words в файл failed_words.txt."""
+    return
+
 
 if __name__ == "__main__":
     words = ["apple", "banana", "orange"]
-    request_to_neural_network(words)
+    table_for_test = request_to_neural_network(words)
+    print(table_for_test)
